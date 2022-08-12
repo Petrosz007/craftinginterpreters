f05149d9787f71546c40aea6c9b19f1b13455795
Andi Péter <andipeti2000@gmail.com>
2022-08-12 18:18:17 +0200

    Add Chapter 15

---
+++ clox/vm.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "chunk.h"
+#include "value.h"
+
+#define STACK_MAX 256
+
+typedef struct {
+  Chunk *chunk;
+  uint8_t *ip;
+  Value stack[STACK_MAX];
+  Value *stackTop;
+} VM;
+
+typedef enum {
+  INTERPRETER_OK,
+  INTERPRETER_COMPILE_ERROR,
+  INTERPRETER_RUNTIME_ERROR
+} InterpretResult;
+
+void initVM();
+void freeVM();
+InterpretResult interpret(Chunk *chunk);
+void push(Value value);
+Value pop();
